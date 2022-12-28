737df60bc2cc639282f365ab513f3a2869479fe1
Anarasty <murloc.ninja16@gmail.com>
2022-12-28 21:19:40 +0200

    count total price on specific book

---
+++ specific_book/main.js
@@ -0,0 +1,14 @@
+const refs = {
+    price: document.querySelector('#price_number'),
+    count: document.querySelector('#count_inp'),
+    totalPrice: document.querySelector('#price_total_number'),
+    form: document.querySelector(".count_box"),
+  };
+  
+  refs.form.onchange = (e) => {
+    e.target.value > 1 && e.target.value <= 42
+      ? (refs.totalPrice.textContent = (
+          Number(refs.price.textContent) * e.target.value
+        ).toFixed(2))
+      : (refs.count.value = 1) && alert("Please enter a value between 0 and 43!");
+  };
