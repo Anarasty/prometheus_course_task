8250d6f3eac51c455ea191ce7815d3be87316027
Anarasty <murloc.ninja16@gmail.com>
2023-01-04 16:07:03 +0200

    default image added in no image in json

--- books_list/main.js
+++ books_list/main.js
@@ -1,5 +1,3 @@
-//console.log(books[0].author);
-
 fetch("products.json")
   .then(function (response) {
     return response.json();
@@ -7,10 +5,12 @@ fetch("products.json")
   .then(function (products) {
     let placeholder = document.querySelector("#book_list_box");
     let out = "";
+    const defaultImage = 'no-img.png';
+
     for (let product of products) {
       out += `
       <div class="book_info">
-        <img src="${product.image}" alt="book">
+        <img src="${product.image.length == 0 ? defaultImage : product.image}" alt="book" id="img">
         <h1 class="book_name">${product.title}</h1>
         <h2 class="book_author">${product.author}</h2>
         <div class="info_container">
